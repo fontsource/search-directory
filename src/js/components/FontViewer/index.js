30d3e85f1176cfe01f96029c03b1e741e71af315
people <jwr12135@gmail.com>
2020-09-30 14:21:21 -0400

    Remove extra divider

--- src/js/components/FontViewer/index.js
+++ src/js/components/FontViewer/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Divider, Toolbar, Typography } from '@material-ui/core';
+import { Toolbar, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Prism from 'prismjs';
 
@@ -65,8 +65,6 @@ export default function FontViewer({ view }) {
           <br />
 
           <Installation fontData={fontData} />
-
-          <Divider classes={{ root: classes.divider }} />
 
           <Footer fontData={fontData} />
         </div>
