4bd6dd18a9818d80f444a2548ccdc3133659e1af
people <jwr12135@gmail.com>
2020-10-05 16:06:43 -0400

    Remove unused fonts.

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../style.css';
-import 'fontsource-roboto/300.css';
-import 'fontsource-roboto/400.css';
-import 'fontsource-roboto/500.css';
-import 'fontsource-roboto/700.css';
+import 'fontsource-roboto/latin-300-normal.css';
+import 'fontsource-roboto/latin-400-normal.css';
+import 'fontsource-roboto/latin-500-normal.css';
+import 'fontsource-roboto/latin-700-normal.css';
 
 import App from './components/App';
 
