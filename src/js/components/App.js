2aecc9367155a6772218163289111447e2b90a7f
people <jwr12135@gmail.com>
2020-11-09 07:58:34 -0500

    implement react JSX transform

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import useEventListener from '@use-it/event-listener';
--- src/js/components/MainView/FontPreview.js
+++ src/js/components/MainView/FontPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Slider, Tooltip, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
