0cc3ca8ee12fb7fde7406c67a4cc0f8db322bd8f
Johnwesley R <jwr12135@gmail.com>
2020-12-28 14:25:20 -0500

    Integrate fontsource v4

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
 
 import '../style.css';
-import 'fontsource-roboto/latin-300-normal.css';
-import 'fontsource-roboto/latin-400-normal.css';
-import 'fontsource-roboto/latin-500-normal.css';
-import 'fontsource-roboto/latin-700-normal.css';
+import '@fontsource/roboto/latin-300.css';
+import '@fontsource/roboto/latin-300-italic.css';
+import '@fontsource/roboto/latin-400.css';
+import '@fontsource/roboto/latin-400-italic.css';
+import '@fontsource/roboto/latin-500.css';
+import '@fontsource/roboto/latin-500-italic.css';
+import '@fontsource/roboto/latin-700.css';
+import '@fontsource/roboto/latin-700-italic.css';
 
 import App from './components/App';
 
